d86c7b1f4c7d9e203c54fd21c6acf3d8e28404c0
Sergio Vera <sergio.vera.h@gmail.com>
2020-07-10 01:43:07 +0200

    test2

--- src/main.rs
+++ src/main.rs
@@ -51,3 +51,11 @@ fn main() {
         Ok(_) => println!("successfully wrote to {}", display),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
